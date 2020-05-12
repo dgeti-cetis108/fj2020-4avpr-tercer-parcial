f86f6a6f90e2a0fe26d79fe6b4431b0d37369588
Bidkar Aragon Cardenas <bidkar@cetis108.edu.mx>
2020-05-12 09:56:01 -0600

    Clase 3.3 - CSS & JS
    - Agregar bulma
    - Incluir en el index a bulma y probar que funcione
    - Agregar jquery
    - Agregar una imagen

--- public/index.php
+++ public/index.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>MyApp - Bienvenido a MyApp (Inicio de sesión)</title>
+    <link rel="stylesheet" href="css/bulma.css">
 </head>
 <body>
     <h1>Hola desde Apache!</h1>
